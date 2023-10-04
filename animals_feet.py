e59708dd34462115402cf2fd250c839b9fadc150
Aubrey Zhang <138344000+auzhangLABS@users.noreply.github.com>
2023-10-04 16:51:41 -0400

    Create animals_feet.py

---
+++ animals_feet.py
@@ -0,0 +1,7 @@
+#this function adds up all the feet from the animals
+def animals(chickens, cows, pigs):
+	chickens_leg = chickens*2
+	cows_leg = cows *4
+	pigs_leg = pigs *4
+	total = chickens_leg + cows_leg + pigs_leg
+	return total
